eeb47d68068a6ee10cc6165e4755803d961bde9d
Aidar Ganeev <Aidar.Ganeev@halliburton.com>
2021-04-03 07:08:26 +0300

    Added word count

--- VICEVERSA/views.py
+++ VICEVERSA/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+import re
 
 def home(request):
 	return render(request, 'home.html')
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	word_count = len(re.findall(r'\w+', user_text))
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'wordcount':word_count})
+
+
+
